805c77ed17a065afc8cfc1c755a4c880ae8bf314
Jim Fowler <fowler@math.osu.edu>
2022-02-11 11:00:28 -0500

    show both original and prediction

--- game.py
+++ game.py
@@ -26,25 +26,31 @@ generator.eval()
 
 def run():
     sdl2.ext.init()
-    window = sdl2.ext.Window("The Pong Game", size=(128,128))
+    window = sdl2.ext.Window("Prediction", size=(128,128))
     window.show()
-    windowsurface = window.get_surface()
-    pixelview = sdl2.ext.pixels3d(windowsurface)
 
-    #with Image.open("output.png") as im:
-    #    pix = np.array(im.getdata()).reshape(im.size[0], im.size[1], 3)
-    #    pix = pix.swapaxes(0,1)
-    #    pixelview[:, :, [2,1,0]] = pix
-    #    window.refresh()
+    ground_window = sdl2.ext.Window("Ground truth", size=(128,128))
+    ground_window.show()
 
     running = True
     action = Action.NOOP
     while running:
-        sleep(0.01)
+        #sleep(0.01)
         ale.act(action)
 
         if ale.game_over():
             ale.reset_game()
+
+        groundsurface = ground_window.get_surface()
+        pixelview = sdl2.ext.pixels3d(groundsurface)
+        pix = ale.getScreenRGB()
+        pix = Image.fromarray(pix)
+        pix = torchvision.transforms.Resize( (ground_window.size[1], ground_window.size[0]),
+                                             torchvision.transforms.InterpolationMode.NEAREST ).forward( pix )
+        pix = np.array(pix).swapaxes(0,1)
+        pixelview[:, :, [2,1,0]] = pix
+
+        ground_window.refresh()
         
         windowsurface = window.get_surface()
         pixelview = sdl2.ext.pixels3d(windowsurface)
@@ -62,14 +68,14 @@ def run():
                                              torchvision.transforms.InterpolationMode.NEAREST ).forward( pix )
         pix = np.array(pix).swapaxes(0,1)
         pixelview[:, :, [2,1,0]] = pix
-        print('looped')
+
         window.refresh()
 
         events = sdl2.ext.get_events()
         for event in events:
             if event.type == sdl2.SDL_KEYDOWN:
                 if event.key.keysym.sym == sdl2.SDLK_UP:
-                    action = Action.UP
+                    action = Action.FIRE
                 elif event.key.keysym.sym == sdl2.SDLK_DOWN:
                     action = Action.DOWN
                 elif event.key.keysym.sym == sdl2.SDLK_RIGHT:
